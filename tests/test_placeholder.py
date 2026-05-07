4dbd236ff2ff68e8ec8ba49e159760da7773d78d
Rob de Beir <rob.de.beir@boskalis.com>
2026-05-07 09:57:35 +0200

    Remove placeholder test and build script for Docker image

--- tests/test_placeholder.py
+++
@@ -1,8 +0,0 @@
-"""Module providing a function to test the test suite."""
-
-# This file is used to test the test suite.
-
-
-def test_placeholder():
-    """Default test to check if the test suite is working."""
-    pass
